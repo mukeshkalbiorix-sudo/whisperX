f4e9c4629fb83d76deae762564d6e0003f0443d0
mukeshkalbiorix-sudo <mukeshk.albiorix@gmail.com>
2025-12-12 15:23:36 +0530

    Change HF_TOKEN to a hardcoded value
    Replaced HF_TOKEN environment variable retrieval with a hardcoded token.

--- handler.py
+++ handler.py
@@ -6,7 +6,7 @@ import torch
 from pyannote.audio import Pipeline  # For diarization
 
 # Env vars (set in RunPod)
-HF_TOKEN = os.getenv("HF_TOKEN")
+HF_TOKEN = os.getenv("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")
 WHISPER_MODEL = os.getenv("WHISPER_MODEL", "large-v3")
 BATCH_SIZE = int(os.getenv("BATCH_SIZE", 16))
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
